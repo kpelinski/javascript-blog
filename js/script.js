ba57c4840f361a44627d26e5b1821d9fb1c1fd3e
Kamil Pelinski <kpelinski@interia.pl>
2021-01-23 16:32:23 +0100

    art act

--- js/script.js
+++ js/script.js
@@ -17,7 +17,11 @@ const titleClickHandler = function(event){
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
 
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
